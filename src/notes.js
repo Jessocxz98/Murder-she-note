4ea2aa25dc87fcd9eadca25a442f0a93269490f8
Jessica Bulman <jessicabulman97@gmail.com>
2021-10-04 17:23:59 +0100

    implementing create note button

--- src/notes.js
+++ src/notes.js
@@ -1,18 +1,19 @@
 class Notes { 
 
   constructor () {
-    this.obj = {} 
-
-
+    this.obj = {}
+    this.arr = [] 
   }
 
+  
   createNotes() {
+    let obj = {}
     let title = prompt("Please enter the title of your note", "Gin and Daggers");
-    this.obj.title = title
+    obj.title = title
+    // this.arr.push(alert(Object.keys(this.obj)));
     let message = prompt("Please enter your message", "There are three things you can never have enough of in life, Lieutenant: chocolate, friends, and the theatre");
-    this.obj.message = message
+    obj.message = message
+    this.arr.push(obj)
   }
-
-
 
 }
