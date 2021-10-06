fc617a4d6088fdfedd9b89da77e79d0776787b2e
Osneil Drakes <osneil-drakes@hotmail.com>
2021-10-06 16:37:14 +0100

    update for emoji

--- interface.js
+++ interface.js
@@ -1,12 +1,40 @@
 document.addEventListener("DOMContentLoaded", () => {
   const notes = new Notes();
+  let arr = notes.notebook.display;
 
   document.querySelector('#create-note').addEventListener('click', () => {
     notes.createNotes();
-    document.querySelector('#display-notes').innerText = notes.notebook.displayNotes();
+    convertToEmoji(arr[arr.length-1].message); 
+    
   })
 
-  
-})
+  function convertToEmoji(text) {
+  fetch('https://makers-emojify.herokuapp.com/', {
+      method: 'POST', 
+      headers: {'Content-Type': 'application/json',
+            },
+      //body: '{"text": "Hello, :earth_africa:"}',
+      body: `{"text": "${text}"}`,
+      }).then((response) => {
+          return response.json()
+        }).then((data) => {
+
+          data.emojified_text
+          console.log(data.emojified_text);
+          submitNote(data.emojified_text);
+
+        });  
+      }
+
+    function submitNote(text) {
+      arr[arr.length-1].message = text;
+      document.querySelector('#display-notes').innerText = notes.notebook.displayNotes();
+    }
+
+
+
+  });
+
+
 
 
