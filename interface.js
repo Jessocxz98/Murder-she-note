5993331cc6d6e243fa4b91036e46d4677811bda2
Osneil Drakes <osneil-drakes@hotmail.com>
2021-10-07 13:48:41 +0100

    formatting js files

--- interface.js
+++ interface.js
@@ -5,33 +5,28 @@ document.addEventListener("DOMContentLoaded", () => {
   document.querySelector('#create-note').addEventListener('click', () => {
     notes.createNotes();
     convertToEmoji(arr[arr.length-1].message); 
-    
   })
 
   function convertToEmoji(text) {
-  fetch('https://makers-emojify.herokuapp.com/', {
+    fetch('https://makers-emojify.herokuapp.com/', {
       method: 'POST', 
       headers: {'Content-Type': 'application/json',
-            },
+      },
       //body: '{"text": "Hello, :earth_africa:"}',
       body: `{"text": "${text}"}`,
-      }).then((response) => {
-          return response.json()
-        }).then((data) => {
+    }).then((response) => {
+      return response.json()
+      }).then((data) => {
+        data.emojified_text
+        console.log(data.emojified_text);
+        submitNote(data.emojified_text);
+      });  
+  }
 
-          data.emojified_text
-          console.log(data.emojified_text);
-          submitNote(data.emojified_text);
-
-        });  
-      }
-
-    function submitNote(text) {
-      arr[arr.length-1].message = text;
-      document.querySelector('#display-notes').innerText = notes.notebook.displayNotes();
-    }
-
-
+  function submitNote(text) {
+    arr[arr.length-1].message = text;
+    document.querySelector('#display-notes').innerText = notes.notebook.displayNotes();
+  }
 
   });
 
--- src/notebook.js
+++ src/notebook.js
@@ -6,12 +6,12 @@ class Notebook {
   }
 
   displayNotes(){
-  let arrayLength = this.display.length;
-  let newarr = [];
-  for (let i = 0; i < arrayLength; i++) {
-    newarr.push(`${this.display[i].title} : ${this.display[i].message}` );
-  } 
-  return newarr.join('\r\n')
+    let arrayLength = this.display.length;
+    let newarr = [];
+    for (let i = 0; i < arrayLength; i++) {
+      newarr.push(`${this.display[i].title} : ${this.display[i].message}` );
+    } 
+    return newarr.join('\r\n')
   }
 
 };
