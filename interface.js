10f77ae8c2f595ffaf753e83fbab3c758886dd4a
Osneil Drakes <osneil-drakes@hotmail.com>
2021-10-08 13:17:01 +0100

    change variable name for array list/link

--- interface.js
+++ interface.js
@@ -1,13 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const notes = new Notes();
+  let notes = new Notes();
   let arr = notes.notebook.display,
+    count = 1,
     container1 = document.querySelector('.container1'),
     container2 = document.querySelector('.container2'),
     fullNote = document.querySelector('#full-note-message');
 
-    container2.style.display = 'none'; // on load the containers/divs are hidden 
+    container2.style.display = 'none';
   
-
   document.querySelector('#create-note').addEventListener('click', () => {
     notes.createNotes();
     convertToEmoji(arr[arr.length-1].message); 
@@ -20,28 +20,31 @@ document.addEventListener("DOMContentLoaded", () => {
       },
       //body: '{"text": "Hello, :earth_africa:"}',
       body: `{"text": "${text}"}`,
-    }).then((response) => {
-      return response.json()
-      }).then((data) => {
-        data.emojified_text
-        console.log(data.emojified_text);
-        submitNote(data.emojified_text);
-      });  
+      }).then((response) => {
+        return response.json()
+          }).then((data) => {
+            data.emojified_text
+            console.log(data.emojified_text);
+            submitNote(data.emojified_text);
+          });  
   }
 
   function submitNote(text) {
     arr[arr.length-1].message = text;
-    document.querySelector('#display-abbreviated-notes').innerText = notes.notebook.displayAbbreviatedNote();
+    //document.querySelector('#display-abbreviated-notes').innerText = notes.notebook.displayAbbreviatedNote();
     createNotesList();
+    count += 1
   }
 
-
-
   function createNotesList() {
-    let abbrArr = notes.notebook.testDisplayAbbreviatedNote()
-    // console.log(abbrArr)
-      let currentArr = arr[arr.length-1];
-        let li = document.createElement('li'),
+    if (count > arr.length) {
+      count = arr.length
+      return; }
+    else {
+      let abbrArr = notes.notebook.testDisplayAbbreviatedNote()
+      console.log(abbrArr)
+      let currArr = arr[arr.length-1];
+      let li = document.createElement('li'),
         link = document.createElement('a'),
         description = `${abbrArr[abbrArr.length-1]}`;
 
@@ -49,25 +52,23 @@ document.addEventListener("DOMContentLoaded", () => {
       (link.href = '#'),
       (link.id = description);
 
-    link.addEventListener('click', (e) => {
-      displayNote(currentArr);
-      e.preventDefault();
-    });
+      link.addEventListener('click', (e) => {
+        displayNote(currArr);
+        e.preventDefault();
+      });
 
-    li.appendChild(link);
-    document.getElementById('created-notes').appendChild(li);
-    //}
+      li.appendChild(link);
+      document.getElementById('created-notes').appendChild(li);
+      }
   }
-
-
+  
   function displayNote(obj) {
     container1.style.display = 'none';
     container2.style.display = 'block';
     fullNote.textContent = obj.message
-    
     document.querySelector('#back-button').addEventListener('click', (e) => {
-    container1.style.display = 'block';
-    container2.style.display = 'none';
+      container1.style.display = 'block';
+      container2.style.display = 'none';
     });
   }
 
--- src/notes.js
+++ src/notes.js
@@ -13,13 +13,15 @@
     let title = prompt("Please enter the title of your note", "Gin and Daggers");
     if (this.arrTitles.includes(title)){
       alert("Ok, you've already used that name. Let's try this process again!");
+    } else if (title === null) {
+      return;
     } else {
-    this.arrTitles.push(title);
-    let message = prompt("Please enter your message", "There are three things you can never have enough of in life, Lieutenant: chocolate, friends, and the theatre");
-    this.submitNote(title,message)
+      this.arrTitles.push(title);
+      let message = prompt("Please enter your message", "There are three things you can never have enough of in life, Lieutenant: chocolate, friends, and the theatre");
+      this.submitNote(title,message)
     };
+      
   }
-
 
   submitNote(title,message) {
     let obj = {}
